19dbec524dd8bf35cd08fb35c56d3ea0956d7560
banachonion <azaxsdaacsxzccds@gmail.com>
2026-09-17 17:55:25 -0400

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,1 +1,4 @@
 console.log("Seems like the page you were looking for is gone. Please try again later.");
+setTimeout(() => {
+  if(location.path=="/"&&document.title.includes("404")&&document.body.innerText.includes("The page you requested could not be found. It may have been moved or removed.")) location.href=location.href;
+})
